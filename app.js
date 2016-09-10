698194877371e0dd8f9976e07735728794349832
cthulh <tryglavson@hotmail.co.uk>
2016-09-11 00:05:16 +0100

    Requiring same object VS a constructor

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ var greeting = require('./greet');
 
 greeting.english();
 greeting.spanish();
+
+var greet2 = require('./greet2');
+greet2.greet();
+greet2.greeting = "Changed!";
+// returns the same object as above
+var greet2b = require('./greet2');
+greet2b.greet();
+// workaround is to export a constructor
+var grtr = require('./greet3');
+var greet3 = new grtr();
+greet3.greet();
+greet3.greeting = "Changed again!";
+var greet4 = new grtr();
+greet4.greet();
---
+++ greet2.js
@@ -0,0 +1,8 @@
+var Greetr = function(){
+  this.greeting = 'Hello World!';
+  this.greet = function(){
+    console.log(this.greeting);
+  };
+};
+
+module.exports = new Greetr();
